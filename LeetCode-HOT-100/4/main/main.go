81a306c3e3c91ed062343c456c76c6d7b2bf15dc
lvhuawei1997 <1451308045@qq.com>
2022-07-16 14:52:34 +0800

    commit

--- LeetCode-HOT-100/4/main/main.go
+++ LeetCode-HOT-100/4/main/main.go
@@ -1,1 +1,53 @@
 package main
+
+import "fmt"
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	totalLength := len(nums1) + len(nums2)
+	if totalLength%2 == 1 {
+		midIndex := totalLength/2 + 1
+		return float64(getKthElement(nums1, nums2, midIndex))
+	} else {
+		midIndex := totalLength / 2
+		return float64(getKthElement(nums1, nums2, midIndex)+getKthElement(nums1, nums2, midIndex+1)) / 2.0
+	}
+}
+
+func getKthElement(nums1, nums2 []int, k int) int {
+	index1, index2 := 0, 0
+	for {
+		if index1 == len(nums1) {
+			return nums2[index2+k-1]
+		}
+		if index2 == len(nums2) {
+			return nums1[index1+k-1]
+		}
+		if k == 1 {
+			return min(nums1[index1], nums2[index2])
+		}
+		half := k / 2
+		newIndex1, newIndex2 := min(index1+half, len(nums1))-1, min(index2+half, len(nums2))-1
+		pivot1, pivot2 := nums1[newIndex1], nums2[newIndex2]
+		if pivot1 <= pivot2 {
+			k -= newIndex1 - index1 + 1
+			index1 = newIndex1 + 1
+		} else {
+			k -= newIndex2 - index2 + 1
+			index2 = newIndex2 + 1
+		}
+	}
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func main() {
+	nums1 := []int{1, 3}
+	nums2 := []int{2}
+	res := findMedianSortedArrays(nums1, nums2)
+	fmt.Println(res)
+}
